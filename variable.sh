65b9e4435ef283feb69511fd2cd3899cd5924986
mettashalini89@gmail.com <CatMatDog@123>
2023-02-22 11:00:34 -0700

    variabe practice

--- variable.sh
+++ variable.sh
@@ -1,3 +1,11 @@
 #variable is a name assigned to data , syntax: var=data, access variable as $var or ${var}
 Date=2022-01-01
 echo -e "\e[32m today date is ${Date}\e[0m"
+
+#all the time we cannot hardcode the data to a variable bcoz data changes dyanamically.
+# we can assign dynamic data by assigning command or arthematic eexpressions to var
+
+# command substitution
+Date=date
+echo ${Date}
+
