20bcb81d92535b22808c0f1e741f555421867630
mettashalini89@gmail.com <CatMatDog@123>
2023-02-22 14:40:16 -0700

    functions

--- functions.sh
+++ functions.sh
@@ -1,7 +1,14 @@
 #declare a function
 xyz(){
   echo "functions practice"
+  echo "first argument $1"
+  echo "second arg $2"
+  echo "all args $*"
+  echo "no of args $#"
 }
 
 #call function
-xyz
+#xyz
+
+#pass args to function
+xyz 123 abc xyz
